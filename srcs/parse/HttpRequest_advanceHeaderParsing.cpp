274df5c9245091d67230f95174aea3f1f4b2a27a
shoko-soeno <happydream0120@gmail.com>
2025-12-06 22:51:41 +0900

    make format, add (void)cur to suppress unused variable warning

--- srcs/parse/HttpRequest_advanceHeaderParsing.cpp
+++ srcs/parse/HttpRequest_advanceHeaderParsing.cpp
@@ -5,10 +5,10 @@
 // size_type is used here because string::npos is of that type
 std::string::size_type HttpRequest::FindEndOfHeader(
     const std::string& payload) {
-  static const std::string delimiter = "\r\n\r\n";
-  std::string::size_type pos = payload.find(delimiter);
+  static const std::string kDelimiter = "\r\n\r\n";
+  std::string::size_type pos = payload.find(kDelimiter);
   if (pos != std::string::npos) {
-    return pos + delimiter.size(); // position after the delimiter
+    return pos + kDelimiter.size();  // position after the kDelimiter
   }
   return std::string::npos;  // not found
 }
@@ -31,25 +31,26 @@ std::string::size_type HttpRequest::FindEndOfHeader(
  *  - progress が BODY に更新される（ヘッダー完了時）
  */
 bool HttpRequest::AdvanceHeaderParsing() {
-  std::string::size_type endOfHeader = FindEndOfHeader(buffer_);
-  if (endOfHeader == std::string::npos) {
+  std::string::size_type end_of_header = FindEndOfHeader(buffer_);
+  if (end_of_header == std::string::npos) {
     return false;  // need more data
   }
-  std::string headerSection =
-      buffer_.substr(0, endOfHeader);  // includes "\r\n\r\n"
+  std::string header_section =
+      buffer_.substr(0, end_of_header);  // includes "\r\n\r\n"
   try {
-    const char* cur = headerSection.c_str();
+    const char* cur = header_section.c_str();
     cur = this->ConsumeMethod(cur);
     cur = this->ConsumeUri(cur);
     cur = this->ConsumeVersion(cur);
     cur = this->ConsumeHeader(cur);
+    (void)cur;  // suppress unused variable warning
   } catch (lib::exception::ResponseStatusException& e) {
     throw;
   } catch (std::exception&) {
     throw lib::exception::ResponseStatusException(
         lib::http::kInternalServerError);
   }
-  buffer_ = buffer_.substr(endOfHeader);
+  buffer_ = buffer_.substr(end_of_header);
   progress_ = kBody;
   return true;
 }
