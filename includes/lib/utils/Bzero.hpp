ac7cad8cbde4faa8f1027acc8a567ef91f4ffb48
yuzu-juice <yuzujuicesec@gmail.com>
2025-12-15 17:22:14 +0900

    add: bzero function

---
+++ includes/lib/utils/Bzero.hpp
@@ -0,0 +1,13 @@
+#ifndef BZERO_HPP_
+#define BZERO_HPP_
+
+#include <cstddef>
+
+namespace lib {
+namespace utils {
+
+void Bzero(void *s, std::size_t n);
+
+}  // namespace utils
+}  // namespace lib
+#endif
