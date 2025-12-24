febe9679698878a9c62f6836c9a78249b90f87dd
yuzu-juice <yuzujuicesec@gmail.com>
2025-12-24 21:06:16 +0900

    readfile

--- srcs/configParser.cpp
+++ srcs/configParser.cpp
@@ -1,4 +1,5 @@
 #include "ConfigParser.hpp"
+#include "lib/utils/ReadFile.hpp"
 
 ConfigParser::ConfigParser() : current_pos_(0), server_configs_(), content("") {
 }
@@ -11,18 +12,5 @@ ConfigParser::~ConfigParser() {
 }
 
 void ConfigParser::LoadFile(const std::string& filename) {
-  struct stat s;
-  if (stat(filename.c_str(), &s) != 0) {
-    throw std::runtime_error("File does not exist: " + filename);
-  }
-  if (S_ISDIR(s.st_mode)) {
-    throw std::runtime_error(filename + " is a directory");
-  }
-  std::ifstream file(filename.c_str());
-  if (!file.is_open()) {
-    throw std::runtime_error("Failed to open file: " + filename);
-  }
-  content.assign(std::istreambuf_iterator<char>(file),
-                 std::istreambuf_iterator<char>());
-  file.close();
+  content = lib::utils::ReadFile(filename);
 }
