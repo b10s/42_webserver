9faf24f8ec0701dfb0419e7b1f03b2d697aa374b
shoko-soeno <happydream0120@gmail.com>
2025-12-25 21:52:14 +0000

    fix lint error

--- includes/lib/utils/file_utils.hpp
+++ includes/lib/utils/file_utils.hpp
@@ -1,4 +1,7 @@
+#ifndef FILE_UTILS_HPP_
+#define FILE_UTILS_HPP_
 #include <sys/stat.h>
+#include <string>
 
 // uri ends with '/' or is a directory
 static bool IsDirectory(const std::string& path) {
@@ -6,3 +9,5 @@ static bool IsDirectory(const std::string& path) {
   if (stat(path.c_str(), &st) != 0) return false;
   return S_ISDIR(st.st_mode);
 }
+
+#endif  // FILE_UTILS_HPP_
