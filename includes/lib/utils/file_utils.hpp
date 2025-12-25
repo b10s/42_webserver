e2b5a263d19282d0352ad83feba87eb9dae324ca
shoko-soeno <happydream0120@gmail.com>
2025-12-26 07:40:10 +0900

    fix lint error

--- includes/lib/utils/file_utils.hpp
+++ includes/lib/utils/file_utils.hpp
@@ -7,7 +7,7 @@
 namespace lib {
 namespace utils {
 // uri ends with '/' or is a directory
-static bool IsDirectory(const std::string& path) {
+bool IsDirectory(const std::string& path) {
   struct stat st;
   if (stat(path.c_str(), &st) != 0) return false;
   return S_ISDIR(st.st_mode);
