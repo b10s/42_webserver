20b83eca162092b3a5657c7e0878bd84c6affe20
yuzu-juice <121703872+yuzu-juice@users.noreply.github.com>
2026-01-05 03:02:09 +0900

    Revert "83 implement file existence and permission checks"

--- tests/request_handler/RequestHandler_ResolveFilesystemPath.test.cpp
+++ tests/request_handler/RequestHandler_ResolveFilesystemPath.test.cpp
@@ -21,10 +21,8 @@ TEST_F(RequestHandlerTest, ResolveFilesystemPath_RootDirectory) {
     HttpRequest request;
     request.SetUri("/");
     RequestHandler handler(config, request);
-
-    std::string out;
-    ASSERT_NO_THROW(out = handler.ResolveFilesystemPath());
-    EXPECT_EQ(out, "/var/www/html/index.html");
+    
+    EXPECT_EQ(handler.ResolveFilesystemPath(), "/var/www/html/index.html");
 }
 
 TEST_F(RequestHandlerTest, ResolveFilesystemPath_SubDirectory) {
