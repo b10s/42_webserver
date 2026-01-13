f2f9b832004ea371423ccac6730e78b2d054b66b
shoko-soeno <happydream0120@gmail.com>
2026-01-13 15:50:59 +0000

    add Config root check

---
+++ srcs/config_parser/ConfigParser_parseRoot.cpp
@@ -0,0 +1,27 @@
+#include "ConfigParser.hpp"
+#include "lib/http/CharValidation.hpp"
+
+void ConfigParser::IsSafeRootPathOrThrow(const std::string& path) {
+  if (path.empty() || path[0] != '/') {
+    throw std::runtime_error("Root path must be an absolute path: " + path);
+  }
+  for (size_t i = 0; i < path.size(); ++i) {
+    if (!lib::http::IsValidHeaderChar(path[i])) {
+      throw std::runtime_error("Root path contains invalid character: " + path);
+    }
+  }
+  if (path.find("..") != std::string::npos) {
+    throw std::runtime_error("Root path must not contain '..': " + path);
+  }
+}
+
+void ConfigParser::ParseRoot(Location* location) {
+  std::string token = Tokenize(content);
+  if (token.empty()) {
+    throw std::runtime_error(
+        "Syntax error: expected root path but got empty token");
+  }
+  IsSafeRootPathOrThrow(token);
+  location->SetRoot(token);
+  ConsumeExpectedSemicolon("root path");
+}
