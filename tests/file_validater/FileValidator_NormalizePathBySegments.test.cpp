330c3dacc58ac5e4559fc4f1676d8a2a2f56974c
shoko-soeno <happydream0120@gmail.com>
2026-01-14 00:48:48 +0000

    add tests

--- tests/file_validater/FileValidator_NormalizePathBySegments.test.cpp
+++ tests/file_validater/FileValidator_NormalizePathBySegments.test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "FileValidator.hpp"
+#include "lib/exception/ResponseStatusException.hpp"
 
 TEST(FileValidatorTest, NormalizePathBySegments_BasicCases) {
   EXPECT_EQ(FileValidator::NormalizePathBySegments("/a/b/c"),
@@ -9,12 +10,6 @@ TEST(FileValidatorTest, NormalizePathBySegments_BasicCases) {
             "/a/b/c");
   EXPECT_EQ(FileValidator::NormalizePathBySegments("/a/b/./c/"),
             "/a/b/c/");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/b/c"),
-  //           "a/b/c");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/./b/c"),
-  //           "a/b/c");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/b/./c/"),
-  //           "a/b/c/");
 }
 
 TEST(FileValidatorTest, NormalizePathBySegments_DotDotCases) {
@@ -22,10 +17,6 @@ TEST(FileValidatorTest, NormalizePathBySegments_DotDotCases) {
             "/a/c");
   EXPECT_EQ(FileValidator::NormalizePathBySegments("/a/../b/c"),
             "/b/c");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/b/../c"),
-  //           "a/c");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/../b/c"),
-  //           "b/c");
 }
 
 TEST(FileValidatorTest, NormalizePathBySegments_EdgeCases) {
@@ -33,18 +24,6 @@ TEST(FileValidatorTest, NormalizePathBySegments_EdgeCases) {
             "/");
   EXPECT_EQ(FileValidator::NormalizePathBySegments("///"),
             "/");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments(""),
-  //           "");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("."),
-  //           "");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("./"),
-  //           "");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("./a/b"),
-  //           "a/b");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("a/b/."),
-  //           "a/b");
-  // EXPECT_EQ(FileValidator::NormalizePathBySegments("././"),
-            // "");
 }
 
 // expect throw for paths that try to escape root
@@ -53,4 +32,19 @@ TEST(FileValidatorTest, NormalizePathBySegments_RejectEscapeRoot) {
   EXPECT_THROW(FileValidator::NormalizePathBySegments("/a/../../b"), std::runtime_error);
   EXPECT_THROW(FileValidator::NormalizePathBySegments("../a/b"), std::runtime_error);
   EXPECT_THROW(FileValidator::NormalizePathBySegments("a/../../b"), std::runtime_error);
+}
+
+// reject empty paths
+TEST(FileValidatorTest, NormalizePathBySegments_RejectEmptyPaths) {
+  EXPECT_THROW(FileValidator::NormalizePathBySegments(""), std::runtime_error);
+}
+
+// reject relative paths
+TEST(FileValidatorTest, NormalizePathBySegments_RejectRelativePaths) {
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("a/b/c"), std::runtime_error);
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("."), std::runtime_error);
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("./"), std::runtime_error);
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("./a/b"), std::runtime_error);
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("a/b/."), std::runtime_error);
+  EXPECT_THROW(FileValidator::NormalizePathBySegments("././"), std::runtime_error);
 }
