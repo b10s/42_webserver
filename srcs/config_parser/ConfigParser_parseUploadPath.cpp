646b556ab51bee398e099e3d43ea398730dd8b63
Shoko Soeno <ssoeno@c5r2s9.42tokyo.jp>
2026-01-15 18:23:15 +0900

    add upload path check

---
+++ srcs/config_parser/ConfigParser_parseUploadPath.cpp
@@ -0,0 +1,13 @@
+#include "ConfigParser.hpp"
+#include "lib/http/CharValidation.hpp"
+
+void ConfigParser::ParseUploadPath(Location* location) {
+  std::string token = Tokenize(content);
+  if (token.empty()) {
+    throw std::runtime_error(
+        "Syntax error: expected upload path but got empty token");
+  }
+  RequireAbsoluteSafePathOrThrow(token, "Upload path");
+  location->SetUploadPath(token);
+  ConsumeExpectedSemicolon("root path");
+}
